8bf4f35b869258f383062dc0cab2c2c278faaece
Anand <ep22btech11012@iith.ac.in>
2024-08-31 02:28:15 +0530

    Updated db

--- backend/db.js
+++ backend/db.js
@@ -5,11 +5,11 @@ const { Pool } = pkg;
 dotenv.config();
 
 const pool = new Pool({
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
-    database: process.env.DB_DATABASE,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
 });
 pool.connect();
 export default pool;
